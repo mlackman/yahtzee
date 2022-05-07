26d6c0d3e18d4d1351ef9e9afcfbee7bcce29b4d
Mika Lackman <mika.lackman@gmail.com>
2022-05-07 08:26:48 +0300

    initial commit

---
+++ yahtzee/features/support/steps.ts
@@ -0,0 +1,9 @@
+import { When, Then } from '@cucumber/cucumber';
+
+When('player opens yahtzee', async () => {
+  return 'pending';
+});
+
+Then('player can start a game', async () => {
+  return 'pending';
+});
---
+++ yahtzee/features/yahtzee.feature
@@ -0,0 +1,6 @@
+Feature: Yahtzee game
+    Single player yahtzee game
+
+    Scenario: Player about to start game
+        When player opens yahtzee
+        Then player can start a game
