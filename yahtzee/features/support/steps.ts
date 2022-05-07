90c90129f266a81f7d645dcd23f913b00331e366
Mika Lackman <mika.lackman@gmail.com>
2022-05-07 16:14:37 +0300

    test passed

--- yahtzee/features/support/steps.ts
+++ yahtzee/features/support/steps.ts
@@ -1,15 +1,30 @@
-import { When, Then } from '@cucumber/cucumber';
-
+import * as selenium from 'selenium-standalone';
+import { BeforeAll, AfterAll, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
 import { Driver } from 'selenium-webdriver/chrome';
 import { Builder, ThenableWebDriver } from 'selenium-webdriver';
+import { ChildProcess } from "child_process";
 
 let browser: ThenableWebDriver;
+let seleniumServerProcess: ChildProcess;
+
+setDefaultTimeout(60000);
+
+BeforeAll(async () => {
+  seleniumServerProcess = await selenium.start();
+});
+
+AfterAll(async () => {
+  await browser.close();
+  seleniumServerProcess.kill();
+});
 
 When('player opens yahtzee', async () => {
-  const browser = await new Builder().forBrowser('chrome').build();
+  browser = new Builder().forBrowser('chrome').build();
   await browser.get('file:///Users/mlackman/projects/yahtzee/build/index.html');
+
 });
 
 Then('player can start a game', async () => {
-  await browser.quit();
+  // Throws error if element not found
+  await browser.findElement({ id: 'start-game-button' });
 });
