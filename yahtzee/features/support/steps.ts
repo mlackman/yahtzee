9d970bf73e16725be7711ab0e264f44005191cdf
Mika Lackman <mika.lackman@gmail.com>
2022-05-07 10:54:54 +0300

    selenium support

--- yahtzee/features/support/steps.ts
+++ yahtzee/features/support/steps.ts
@@ -1,9 +1,15 @@
 import { When, Then } from '@cucumber/cucumber';
 
+import { Driver } from 'selenium-webdriver/chrome';
+import { Builder, ThenableWebDriver } from 'selenium-webdriver';
+
+let browser: ThenableWebDriver;
+
 When('player opens yahtzee', async () => {
-  return 'pending';
+  const browser = await new Builder().forBrowser('chrome').build();
+  await browser.get('file:///Users/mlackman/projects/yahtzee/build/index.html');
 });
 
 Then('player can start a game', async () => {
-  return 'pending';
+  await browser.quit();
 });
